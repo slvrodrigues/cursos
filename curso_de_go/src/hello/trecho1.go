1e116366761766c5bd358df8084c993ba9dd762e
slvrodrigues <emerson.rodrigues@outlook.com>
2020-02-14 17:13:39 -0300

    curso - de - go

--- curso_de_go/src/hello/trecho1.go
+++ curso_de_go/src/hello/trecho1.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var nome string = "SlvRodrigues"
-	var idade int = 24
-	var versao float32 = 1.1
+	nome := "SlvRodrigues"
+	idade := 24
+	versao := 1.1
 	fmt.Println("Olá, Sr.", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
 
