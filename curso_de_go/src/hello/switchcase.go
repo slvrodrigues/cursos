0c309228735744e25b9d72d23371d96a19646946
slvrodrigues <emerson.rodrigues@outlook.com>
2020-02-19 11:58:23 -0300

    curso - de - go

---
+++ curso_de_go/src/hello/switchcase.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "SlvRodrigues"
+	idade := 24
+	versao := 1.1
+	fmt.Println("Olá, Sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	// fmt.Println("O endereço da minha variável comando é", &comando)
+	// fmt.Println("O comando escolhido foi", comando)
+
+	//   if comando == 1 {
+	//	 fmt.Println("Monitorando...")
+	// } else if comando == 2 {
+	//	fmt.Println("Exibindo Logs...")
+	// } else if comando == 0 {
+	//	fmt.Println("Saindo do programa")
+	// } else {
+	//	fmt.Println("Não conheço este comando")
+	// }
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+	default:
+		fmt.Println("Não conheço este comando")
+	}
+}
