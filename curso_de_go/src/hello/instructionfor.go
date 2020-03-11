569df495620d13087b5a37a9e35f669320d84b67
slvrodrigues <emerson.rodrigues@outlook.com>
2020-03-11 17:11:52 -0300

    curso - de - go

---
+++ curso_de_go/src/hello/instructionfor.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt" //pacote responsavel por fazer requisições web
+	"net/http"
+	"os" //pacote responsavel por informar a saída do comando ao sistema, status 0 1
+)
+
+func main() {
+
+	exibeIntroducao()
+
+	for { //A linguagem go não possuí while, dessa forma o programa entrara em loop usando for
+
+		exibeMenu()
+
+		comando := leComando() //comando será o retorno da função leComando
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0) //saída com sucesso
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1) //saída com tratamento de erro
+		}
+	}
+}
+
+func exibeIntroducao() { //funcao que não retorna nada, apenas exibe
+	nome := "Slvrodrigues"
+	idade := 34
+	versao := 1.1
+	fmt.Println("Olá, Sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+}
+
+func leComando() int {
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O comando escolhido foi", comando)
+
+	return comando
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+}
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	sites := []string{"https://www.globo.com", "https://www,terra.com.br", "https://www.caelum.com.br", "https://www.hotmail.com"}
+
+	fmt.Println(sites)
+
+	for i, site := range sites {
+		fmt.Println("Estou passando na posição", i, "do meu slice e essa posição tem o site:", site)
+	}
+
+	//for i := 0; i < len(sites); i++ { //variavel i percorre o tamanho do slice e incrementa até concluir
+	//	fmt.Println(sites[i])
+	//}
+
+	site := "https://r7.com"
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+
+	} else {
+		fmt.Println("Site:", site, "Está com problemas. Status Code:", resp.StatusCode)
+	}
+}
