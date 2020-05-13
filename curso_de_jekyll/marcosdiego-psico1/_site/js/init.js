806a24a4e19bb13d0ba499b1091805e93371483a
slvrodrigues <emerson.rodrigues@outlook.com>
2020-05-13 18:04:41 -0300

    projeto-marcos

--- curso_de_jekyll/marcosdiego-psico1/_site/js/init.js
+++ curso_de_jekyll/marcosdiego-psico1/_site/js/init.js
@@ -8,12 +8,12 @@
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			global: { href: '/marcosdiegopsicologo/css/style.css', containers: '45em', grid: { gutters: ['2em', 0] } },
-			xlarge: { media: '(max-width: 1680px)', href: '/marcosdiegopsicologo/css/style-xlarge.css' },
-			large: { media: '(max-width: 1280px)', href: '/marcosdiegopsicologo/css/style-large.css', containers: '42em', grid: { gutters: ['1.5em', 0] }, viewport: { scalable: false } },
-			medium: { media: '(max-width: 1024px)', href: '/marcosdiegopsicologo/css/style-medium.css', containers: '85%!' },
-			small: { media: '(max-width: 736px)', href: '/marcosdiegopsicologo/css/style-small.css', containers: '90%!', grid: { gutters: ['1.25em', 0] } },
-			xsmall: { media: '(max-width: 480px)', href: '/marcosdiegopsicologo/css/style-xsmall.css' }
+			global: { href: 'https://marcosdiegopsicologo.github.io/home/css/style.css', containers: '45em', grid: { gutters: ['2em', 0] } },
+			xlarge: { media: '(max-width: 1680px)', href: 'https://marcosdiegopsicologo.github.io/home/css/style-xlarge.css' },
+			large: { media: '(max-width: 1280px)', href: 'https://marcosdiegopsicologo.github.io/home/css/style-large.css', containers: '42em', grid: { gutters: ['1.5em', 0] }, viewport: { scalable: false } },
+			medium: { media: '(max-width: 1024px)', href: 'https://marcosdiegopsicologo.github.io/home/css/style-medium.css', containers: '85%!' },
+			small: { media: '(max-width: 736px)', href: 'https://marcosdiegopsicologo.github.io/home/css/style-small.css', containers: '90%!', grid: { gutters: ['1.25em', 0] } },
+			xsmall: { media: '(max-width: 480px)', href: 'https://marcosdiegopsicologo.github.io/home/css/style-xsmall.css' }
 		},
 		plugins: {
 			layers: {
