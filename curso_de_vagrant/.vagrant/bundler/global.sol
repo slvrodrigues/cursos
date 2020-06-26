f2797188508e682e149c333954edba86ef46821c
slvrodrigues <emerson.rodrigues@outlook.com>
2020-06-26 16:59:20 -0300

    curso_de_docker_alura

---
+++ curso_de_vagrant/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-disksize",["= 0.1.3"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.24.0"]]],"checksum":"ae8080ae61c5ab8fb46bda419836855b254fe6554116f0b1713c520aa1b75070","vagrant_version":"2.2.9"}
